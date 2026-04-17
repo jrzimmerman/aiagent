ebc53141db592ccd95fa2347d91225d562016661
Justin Zimmerman <zimmy130@gmail.com>
2026-04-17 16:05:15 -0400

    add get_file_content and tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,22 +5,21 @@ def get_files_info(working_directory, directory="."):
     try:
         working_dir_abs = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-        # Will be True or False
+
         valid_target_dir = (
             os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
         )
         if not valid_target_dir:
-            raise ValueError(
-                f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-            )
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(target_dir):
-            raise ValueError(f'Error: Directory "{directory}" does not exist')
-
-        contents = os.listdir(target_dir)
-        for item in contents:
+            return f'Error: Directory "{directory}" does not exist'
+        files_info = []
+        files = os.listdir(target_dir)
+        for item in files:
             f = os.path.join(target_dir, item)
             file_size = os.path.getsize(f)
             is_dir = os.path.isdir(f)
-            print(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+            files_info.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+        return "\n".join(files_info)
     except Exception as e:
-        print(f"Error: {e}")
+        return f"Error: {e}"
